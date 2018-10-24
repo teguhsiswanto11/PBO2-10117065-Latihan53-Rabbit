b0148587cdd81114e0b9b0c3a5cb0094a7e96a1f
teguhsiswanto11 <tsiswanto73@gmail.com>
2018-10-24 10:31:05 +0700

    initial commit

--- src/Rabbit.java
+++ src/Rabbit.java
@@ -3,7 +3,7 @@ public class Rabbit extends Animal{
     private String name;
 
     public Rabbit(String name, boolean veg, String food, int legs, String color) {
-        super(veg,food,legs);
+        super(veg,food,legs); //lakukan hal tambahan -> parameter yg berasal dari superClass
         this.name = name;
         this.vegetarian = veg;
         this.eats = food;
